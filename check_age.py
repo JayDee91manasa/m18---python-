60bce86669c35a39f0f14f354fe377c0699ee0b4
JayDee91manasa <jdmanasa.codingal@gmail.com>
2025-07-23 13:02:19 +0530

    Add files via upload

---
+++ check_age.py
@@ -0,0 +1,13 @@
+age = int(input("how old are you?:"))
+if age >10:
+    if age>=10 and age<=20:
+        print("you are allowed in class age limit in between 10 and 20")
+    else:
+        print("you are not allowed in class")
+else:
+    print("you are too young for the class")
+    import math
+number=int(input("tell a no"))
+power=int(input("enter the power no"))
+print(math.pow(number,power))
+
